08ab4d9c3315b1c8c7c477be23d913013e1c7585
sahithisolasa <solasa.vasavi169@gmail.com>
2025-03-11 18:33:36 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat(history=[])
 
 st.title("🤖 Chatbot - Your AI Assistant")
-st.write("This is a chatbot made by Sreejitha.......")
+st.write("This is a chatbot made by vasavi.......")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
